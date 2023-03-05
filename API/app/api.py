8790b427eefc6eafca1df137654d0f908b1c2aea
a-khanna <akhanna992@gmail.com>
2023-03-05 23:55:27 +0530

    Add an endpoint to accept image file

--- API/app/api.py
+++ API/app/api.py
@@ -1,7 +1,9 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, File, UploadFile
 from fastapi.middleware.cors import CORSMiddleware
+from PIL import Image
+from io import BytesIO
 
-app = FastAPI()
+app = FastAPI(title="Rate my snack API", description="API for rating food images", version="0.1.0")
 
 origins = [
     "http://localhost:3000",
@@ -16,6 +18,9 @@ app.add_middleware(
         allow_headers=["*"]
 )
 
-@app.get("/", tags=["root"])
-async def read_root() -> dict:
-    return {"message": "Welcome!"}
+@app.post("/rate-image", tags=["Evaluate"])
+async def rate_food_item(file: UploadFile = File(...)):
+    image_bytes = await file.read()
+    image = Image.open(BytesIO(image_bytes))
+    width, height = image.size
+    return {"width": width, "height": height}
