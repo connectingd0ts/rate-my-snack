6e74dbeb194c1d0a6ab1ba1af03724fbbe4ea65b
a-khanna <akhanna992@gmail.com>
2023-03-09 14:52:29 +0530

    Add authentication to API

--- API/app/api.py
+++ API/app/api.py
@@ -1,9 +1,11 @@
-from fastapi import FastAPI, File, UploadFile
+from fastapi import FastAPI, UploadFile, Depends
 from fastapi.middleware.cors import CORSMiddleware
+from app.authentication import auth_router, User, get_current_active_user
 from PIL import Image
 from io import BytesIO
 
 app = FastAPI(title="Rate my snack API", description="API for rating food images", version="0.1.0")
+app.include_router(auth_router)
 
 origins = [
     "http://localhost:3000",
@@ -19,7 +21,7 @@ app.add_middleware(
 )
 
 @app.post("/rate-image", tags=["Evaluate"])
-async def rate_food_item(file: UploadFile):
+async def rate_food_item(file: UploadFile, current_user: User = Depends(get_current_active_user)):
     image_bytes = await file.read()
     image = Image.open(BytesIO(image_bytes))
     width, height = image.size
---
+++ API/app/authentication.py
@@ -0,0 +1,121 @@
+from datetime import datetime, timedelta
+from typing import Union
+
+from fastapi import Depends, APIRouter, HTTPException, status
+from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
+from jose import JWTError, jwt
+from pydantic import BaseModel
+
+SECRET_KEY = "1b50a7b4fe4914e8976e863cf60b8ffc3809d32671ccd0e333789321814fbdf5"
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = 60
+
+fake_users_db = {
+    "johndoe": {
+        "username": "johndoe",
+        "full_name": "John Doe",
+        "email": "johndoe@example.com",
+        "password": "123",
+        "disabled": False,
+    }
+}
+
+class Token(BaseModel):
+    access_token: str
+    token_type: str
+
+
+class TokenData(BaseModel):
+    username: Union[str, None] = None
+
+
+class User(BaseModel):
+    username: str
+    email: Union[str, None] = None
+    full_name: Union[str, None] = None
+    disabled: Union[bool, None] = None
+
+
+class UserInDB(User):
+    password: str
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+
+auth_router = APIRouter()
+
+def get_user(db, username: str):
+    if username in db:
+        user_dict = db[username]
+        return UserInDB(**user_dict)
+
+
+def authenticate_user(fake_db, username: str, password: str):
+    user = get_user(fake_db, username)
+    if not user:
+        return False
+    if password != user.password:
+        return False
+    return user
+
+
+def create_access_token(data: dict, expires_delta: Union[timedelta, None] = None):
+    to_encode = data.copy()
+    if expires_delta:
+        expire = datetime.utcnow() + expires_delta
+    else:
+        expire = datetime.utcnow() + timedelta(minutes=15)
+    to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    return encoded_jwt
+
+
+async def get_current_user(token: str = Depends(oauth2_scheme)):
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Could not validate credentials",
+        headers={"WWW-Authenticate": "Bearer"},
+    )
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        username: str = payload.get("sub")
+        if username is None:
+            raise credentials_exception
+        token_data = TokenData(username=username)
+    except JWTError:
+        raise credentials_exception
+    user = get_user(fake_users_db, username=token_data.username)
+    if user is None:
+        raise credentials_exception
+    return user
+
+
+async def get_current_active_user(current_user: User = Depends(get_current_user)):
+    if current_user.disabled:
+        raise HTTPException(status_code=400, detail="Inactive user")
+    return current_user
+
+
+@auth_router.post("/token", response_model=Token, tags=["Authentication"])
+async def login_for_access_token(form_data: OAuth2PasswordRequestForm = Depends()):
+    user = authenticate_user(fake_users_db, form_data.username, form_data.password)
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Incorrect username or password",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+    access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    access_token = create_access_token(
+        data={"sub": user.username}, expires_delta=access_token_expires
+    )
+    return {"access_token": access_token, "token_type": "bearer"}
+
+
+@auth_router.get("/users/me/", response_model=User, tags=["Authentication"])
+async def read_users_me(current_user: User = Depends(get_current_active_user)):
+    return current_user
+
+
+@auth_router.get("/users/me/items/", tags=["Authentication"])
+async def read_own_items(current_user: User = Depends(get_current_active_user)):
+    return [{"item_id": "Foo", "owner": current_user.username}]
